fb64ffe9b70c169fabc2d85ae7e8fb7694caa3f6
daanishsuhail <daanishsuhail.mail@gmail.com>
2024-05-14 22:22:13 -0400

    Added program1_2.c basecode.

---
+++ ch1code/program1_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+ * program1_2.c: A program meant to generate primes by seeing if a number
+ * divides nicely by an integer smaller than half of its value.
+ */
+int main()
+{
+    return 0;
+}
