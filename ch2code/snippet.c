300224bd08a2b51e5ab344f7b13514c30aef0436
daanishsuhail <daanishsuhail.mail@gmail.com>
2024-06-01 12:43:53 -0400

    Added code to snippet. Commented out trigraph sections.

--- ch2code/snippet.c
+++ ch2code/snippet.c
@@ -3,12 +3,32 @@
  * of C described in Ch.2 are tested.
  */
 
-??= include <stdio.h>
+/* ??= include <stdio.h>
 ??= define PI 3.1415
 
 int main()
 ??<
     printf("Hello world\n");
-??>
+??>*/
 // Some code which was designed to work with trigraphs with gcc -trigraphs
 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    /* A simple example showcasing line breaks in code. */
+    int a, fred, bill, this, that = 0;
+    /* Long line below */
+    a = fred + bill * ((this / that) * sqrt(3.14159));
+    /* Same line */
+    a = fred + bill *
+                   ((this / that) * sqrt(3.14159));
+
+    /* Example using \'newline' or 'backslash and newline; */
+
+    printf("First line"
+           "second line");
+
+    return 0;
+}
