597623d27b97264ebdf155b49638220c0b82a201
daanishsuhail <daanishsuhail.mail@gmail.com>
2024-05-31 21:24:02 -0400

    Added snippet.c which contains snippets for chapter 2. Code utilizes trigraphs.

---
+++ ch2code/snippet.c
@@ -0,0 +1,14 @@
+/**
+ * @file snippet.c: This is the snippet document for Ch. 2 where all features
+ * of C described in Ch.2 are tested.
+ */
+
+??= include <stdio.h>
+??= define PI 3.1415
+
+int main()
+??<
+    printf("Hello world\n");
+??>
+// Some code which was designed to work with trigraphs with gcc -trigraphs
+
